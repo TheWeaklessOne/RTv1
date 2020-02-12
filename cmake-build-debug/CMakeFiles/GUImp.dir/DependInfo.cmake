e49044a18b1011895f1dc4f9a66c6fdc632192ed
wstygg <wstygg@student.21-school.ru>
2020-02-12 19:08:28 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/GUImp.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/wstygg/Desktop/RTv1/Sources/main.c" "/Users/wstygg/Desktop/RTv1/cmake-build-debug/CMakeFiles/GUImp.dir/Sources/main.c.o"
+  "/Users/wstygg/Desktop/RTv1/Sources/sdl_utill.c" "/Users/wstygg/Desktop/RTv1/cmake-build-debug/CMakeFiles/GUImp.dir/Sources/sdl_utill.c.o"
+  "/Users/wstygg/Desktop/RTv1/Utils/ft_1.c" "/Users/wstygg/Desktop/RTv1/cmake-build-debug/CMakeFiles/GUImp.dir/Utils/ft_1.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/Users/wstygg/Library/Frameworks/SDL2.framework/Headers"
+  "/Users/wstygg/Library/Frameworks/SDL2_ttf.framework/Headers"
+  "/Users/wstygg/Library/Frameworks/SDL2_image.framework/Headers"
+  "../Includes"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
